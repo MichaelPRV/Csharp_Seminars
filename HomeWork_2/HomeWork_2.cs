262eb461a89e969102638645be13711a7968dc83
MichaelPV <michelin7@rambler.ru>
2022-10-07 17:14:27 +0300

    Task 1 is done

--- HomeWork_2/HomeWork_2.cs
+++ HomeWork_2/HomeWork_2.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int TakeSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = (num / 10) - firstDigit*10;
+
+    return secondDigit;
+}
+
+Console.Write("Input 3-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    int digit2 = TakeSecondDigit(num);
+    Console.WriteLine($"Second digit is {digit2}");
+}
+else
+{
+    Console.WriteLine("Number is not correct. Please input 3-digit number");
+}
+
+
