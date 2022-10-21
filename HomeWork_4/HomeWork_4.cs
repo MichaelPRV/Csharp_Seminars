e585325348015be687150a08b3c7a6e290adcd7c
MichaelPV <michelin7@rambler.ru>
2022-10-21 15:25:43 +0300

    Task 1 is corrected

--- HomeWork_4/HomeWork_4.cs
+++ HomeWork_4/HomeWork_4.cs
@@ -1,9 +1,17 @@
 ﻿
 //Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-/*
-double RaiseToDegree(int n1, int n2)
+
+int RaiseToDegree(int n, int m)
 {
-    double result = Math.Pow(n1,n2);
+    int result = 1;
+    int step = 1;
+
+    while (step <= m)
+    {
+        result *= n;
+        step++;
+    }
+    
     return result;
 }
 
@@ -12,9 +20,12 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number B: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-double toDegree = RaiseToDegree(a,b);
+int toDegree = RaiseToDegree(a,b);
 Console.WriteLine($"{a} to the {b} degree is {toDegree}");
-*/
+
+//Проверка результата через готовую функцию:
+double resultCheck = Math.Pow(a,b);
+Console.WriteLine(resultCheck);
 
 
 //Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -42,7 +53,7 @@ Console.WriteLine($"The sum of number {n} digits is {sumOfDigits}");
 
 
 //Задача 3: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-
+/*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] newArray = new int[size];
@@ -68,3 +79,4 @@ int max = 9;
 
 int[] mArray = CreateRandomArray(size, min, max);
 ShowArray(mArray);
+*/
